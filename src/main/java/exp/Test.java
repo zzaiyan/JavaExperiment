69dd2720671d3d8c0006d3c28818c4dc7f100736
zzaiyan <zzaiyan@outlook.com>
2023-07-30 00:20:39 +0800

    update sth

--- src/main/java/exp/Test.java
+++ src/main/java/exp/Test.java
@@ -1,21 +1,31 @@
 package exp;
 
+import java.util.Collections;
+
 public class Test {
     public static void main(String[] args) {
-        Parent p = new Parent(1);
-        p.print();
-        p = new Child(1);
-        p.print();
+//        Parent p = new Parent(1);
+//        p.print();
+//        System.out.println(p.getName());
 
-        String s1 = "hello" + "world";
-        String s2 = new String(s1);
-        System.out.println(s1 == s2);
-        System.out.println(s1.equals(s2));
+        Child c = new Child(1);
+        c.print();
+        System.out.println(c.getName());
+        System.out.println(c.name);
+//        int[] arr = new int[10];
+//        Collections.sort(arr);
+
+        Parent p = c;
+        p.print();
+        System.out.println(p.getName());
+        System.out.println(p.name);
+
     }
 }
 
 class Parent {
-    int m;
+    int m = 0;
+    String name = "Parent's Name";
 
     public Parent(int x) {
         System.out.println("Parent's constructor");
@@ -24,14 +34,23 @@ class Parent {
     public void print() {
         System.out.println("Parent");
     }
+
+    public String getName() {
+        return this.name;
+    }
 }
 
 class Child extends Parent {
-    double m;
+    String name = "Child's Name";
+    double m = 1.5;
 
     public Child(int x) {
         super(x);
         System.out.println("Child's constructor");
+    }
+
+    public String getName() {
+        return this.name;
     }
 
     public void print() {
